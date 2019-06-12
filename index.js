f852d60ef4a36e7d86d45d75291e513f2a3fe4f3
himanshu shukla <himanshushukla16398@gmail.com>
2019-06-12 18:52:06 +0530

    learnt about callbacks and closures and error handlers

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var rect=require('./rectangle.js');
 function solveRect(l,b){
     console.log("solving for are and perimeter of a rectangle with length l="+l+"and breadth b="+b);
-    if(l<=00||b<=0)
-    console.log("invalid parameters");
-    else
-    {
-        console.log("perimeter is "+rect.perimeter(l,b)+"area is "+rect.area(l,b));
-    }
-}
+    rect(l,b,(err,rectangle) => {
+        if(err)
+        console.log("ERROR: "+err.message);
+        else
+        {
+        console.log("the area of the rectangle with dimensions l= "+l+"b= "+b+" is "+rectangle.area());
+        console.log("the perimeter of the rectangle with dimensions l= "+l+"b= "+b+" is "+rectangle.perimeter());
+        }
+    });
+    console.log("this part is executed before the above calculation");
+};
 solveRect(3,4);
 solveRect(4,4);
 solveRect(-3,4);
