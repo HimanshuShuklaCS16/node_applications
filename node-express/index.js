4c4fd608224f1ba83121e7f2ff085693c0c1f596
himanshu shukla <himanshushukla16398@gmail.com>
2019-06-13 19:40:41 +0530

    static html files served

--- node-express/index.js
+++ node-express/index.js
@@ -3,8 +3,11 @@ const http=require("http");
 const hostname="localhost";
 const port=3000;
 const app=express();
+const morgan=require("morgan");
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + "/public"));
 app.use((req,res,next) => {
-console.log(req.headers);
 res.statusCode=200;
 res.setHeader('content-Type','text/html');
 res.end(`<html><body><h1>this is an express server</h1></body></html>`);
