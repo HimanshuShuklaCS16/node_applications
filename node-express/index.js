5b1b6def5f93d7065190c25d671e02b37cca3e82
himanshu shukla <himanshushukla16398@gmail.com>
2019-06-14 21:56:46 +0530

    rest operations implemented in express server

--- node-express/index.js
+++ node-express/index.js
@@ -4,6 +4,48 @@ const hostname="localhost";
 const port=3000;
 const app=express();
 const morgan=require("morgan");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.all('/dishes',(req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('content-Type','text/plain');
+    next();
+});
+app.get('/dishes',(req,res,next) => {
+    res.end("will send all the dishes to you");
+});
+app.post('/dishes',(req,res,next) => {
+    res.end("will add dish with name : " + req.body.name + " and description " + req.body.description);
+});
+app.put('/dishes',(req,res,next) => {
+    res.statusCode = 403;
+    res.end("put operation not allowed on /dishes");
+});
+app.delete('/dishes',(req,res,next) => {
+    res.end("will delete all the dishes !!");
+});
+
+app.all('/dishes/:dishid',(req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('content-Type','text/plain');
+    next();
+});
+app.get('/dishes/:dishid',(req,res,next) => {
+    res.end("will get dish with id " + req.params.dishid);
+});
+app.post('/dishes/:dishid',(req,res,next) => {
+    res.statusCode = 403;
+    res.end("post operation not supported on /dishes/" + req.params.dishid);
+});
+app.put('/dishes/:dishid',(req,res,next) => {
+    res.end("will update dish with id : " +req.params.dishid + "with name "+ req.body.name + " and description " + req.body.description);
+});
+app.delete('/dishes/:dishid',(req,res,next) => {
+    res.end("deleting dish with id " + req.params.dishid);
+});
+
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + "/public"));
