7ca3a36412c17b8848617bd6754630fd92a709c9
HimanshuShuklacs16 <41748625+HimanshuShuklaCS16@users.noreply.github.com>
2019-07-09 11:41:36 +0530

    delete config.js

--- conFusionserver/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion', 
-    'facebook' :{
-        clientId : '2326902410760652',
-        clientSecret : '6f70bef3fb88ed7c887e3a2cb2afd700'
-    }
-}
